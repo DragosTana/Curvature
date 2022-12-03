81b6d056099e86a42e5e73ee4a24637ecc0d85b0
Dragos <dragos.tanasa99@gmail.com>
2022-12-03 14:36:21 +0100

    added calculate_curvature_poly function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 import circle_fit as cf
 
+
 def plot_track(data):
     x, y = data.T
     fig, ax = plt.subplots()
@@ -12,11 +13,13 @@ def plot_track(data):
     ax.scatter(x, y, marker=".")
     plt.show()
 
+
 def plot_curvature(curvature):
     x = list(range(len(curvature)))
     fig, ax = plt.subplots()
     ax.plot(x, curvature)
     plt.show()
+
 
 def load_csv(file_name):
     data = []
@@ -27,23 +30,44 @@ def load_csv(file_name):
             for word in line:
                 point.append(float(word))
             data.append(point)
+    data = np.array(data)
     return data
 
-def calculate_curvature(track, n):
+
+def calculate_curvature_circle(track, n):
+    # calculate the curvature using a least squares circle passing through 2n+1 points
     curvature = []
     for i in range(n, len(track)-n):
         points = track[i-n:i+n+1]
-        _,_,r,_ = cf.least_squares_circle(points)
+        _, _, r, _ = cf.least_squares_circle(points)
         curvature.append(1/r)
-
     return curvature
+
+
+def calcluate_curvature_poly(track, n):
+    # calculate the curvature using a least square polynomial passing through 2n+1 points
+    curvature = []
+    for i in range(n, len(track) - n):
+        points = track[i - n:i + n + 1]
+        xp, yp = points.T
+        tx = list(range(len(xp)))
+        ty = list(range(len(yp)))
+        x_coeff = np.polyfit(xp, tx, 2)  # list of a_2, a_1, a_0
+        y_coeff = np.polyfit(yp, ty, 2)  # list of b_2, b_1, b_0
+        # print(xp, yp, tx, ty, x_coeff, y_coeff)
+        k = (2*(x_coeff[1]*y_coeff[2]-x_coeff[2]*y_coeff[1]))/(((x_coeff[1]**2)+(y_coeff[1]**2))**(3/2))
+        curvature.append(k)
+    return curvature
+
 
 def main():
     data = load_csv("Pista.csv")
-    data = np.array(data)
     plot_track(data)
-    curvature = calculate_curvature(data, 2)
-    plot_curvature(curvature)
-    
+    # curvature1 = calculate_curvature_circle(data, 2)
+    # plot_curvature(curvature1)
+    curvature2 = calcluate_curvature_poly(data, 2)
+    plot_curvature(curvature2)
+
+
 main()
 
