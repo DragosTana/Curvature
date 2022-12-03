84c784336ca1c16d373d874ee2a75cc113f38720
Dragos <dragos.tanasa99@gmail.com>
2022-12-03 12:29:03 +0100

    functioning program

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
-
-def plot_data_2D(data):
+import circle_fit as cf
+def plot_track(data):
     x, y = data.T
     fig, ax = plt.subplots()
     ax.set_title("Dati")
@@ -10,7 +10,11 @@ def plot_data_2D(data):
     ax.set_ylabel("y")
     ax.scatter(x, y, marker=".")
     plt.show()
-
+def plot_curvature(curvature):
+    x = list(range(len(curvature)))
+    fig, ax = plt.subplots()
+    ax.plot(x, curvature)
+    plt.show()
 def load_csv(file_name):
     data = []
     with open(file_name, 'r') as csv_file:
@@ -22,10 +26,21 @@ def load_csv(file_name):
             data.append(point)
     return data
 
+def calculate_curvature(track, n):
+    curvature = []
+    for i in range(n, len(track)-n):
+        points = track[i-n:i+n+1]
+        _,_,r,_ = cf.least_squares_circle(points)
+        curvature.append(1/r)
+
+    return curvature
+
+
 def main():
     data = load_csv("Pista.csv")
     data = np.array(data)
-    plot_data_2D(data)
-
+    plot_track(data)
+    curvature = calculate_curvature(data, 2)
+    plot_curvature(curvature)
 main()
 
